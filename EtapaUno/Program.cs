b7642a016bacff14d8ee5c12a92fab1ae54bb5e0
Carol Arias <dayarias@gmail.com>
2019-04-22 14:43:10 -0500

    First Saving Relase

--- EtapaUno/Program.cs
+++ EtapaUno/Program.cs
@@ -7,60 +7,86 @@ namespace EtapaUno
     {
         static void Main(string[] args)
         {
-            var escuela= new Escuela("Platzi Academy", 2012, TiposEscuela.Primaria, ciudad: "Bogotá");
+            var escuela = new Escuela("Platzi Academy", 2012, TiposEscuela.Primaria, ciudad: "Bogotá");
             //escuela.Pais ="Colombia";
             //escuela.Ciudad="Bogota";
             var arregloCursos = new Curso[3];
 
-            arregloCursos[0]= new Curso(){
-              Nombre ="101"
+            arregloCursos[0] = new Curso()
+            {
+                Nombre = "101"
             };
 
-            arregloCursos[1]= new Curso(){ 
-                 Nombre ="201"
+            arregloCursos[1] = new Curso()
+            {
+                Nombre = "201"
             };
 
-            arregloCursos[2]= new Curso(){ 
-                 Nombre ="301"
+            arregloCursos[2] = new Curso()
+            {
+                Nombre = "301"
             };
 
 
 
-/* Sin Arreglos
-            var curso2= new Curso(){
-            Nombre ="201"
-            };
+            /* Sin Arreglos
+                        var curso2= new Curso(){
+                        Nombre ="201"
+                        };
 
-            var curso3= new Curso(){
-              Nombre ="301"
-            };
- */
+                        var curso3= new Curso(){
+                          Nombre ="301"
+                        };
+             */
             Console.WriteLine(escuela);
             System.Console.WriteLine("---------------");
-            ImprimirCursos(arregloCursos);
+            ImprimirCursosWhile(arregloCursos);
+            System.Console.WriteLine("---------------");
+            ImprimirCursosDoWhile(arregloCursos);
+            System.Console.WriteLine("---------------");
+            ImprimirCursosFor(arregloCursos);
+
         }
-            private static void ImprimirCursosWhile( Curso [] arregloCursos)
+        private static void ImprimirCursosFor(Curso[] arregloCursos)
+        {
+            for (int i = 0; 1 < arregloCursos.Length; i++)
             {
-                int contador = 0;
-                while (contador < arregloCursos.Length)
-                {
-                    Console.WriteLine($"Nombre {arregloCursos[contador].Nombre}, id{arregloCursos[contador].UniqueId}");
-                    contador ++;
-                }
+                Console.WriteLine($"Nombre {arregloCursos[i].Nombre}, id {arregloCursos[i].UniqueId}");
             }
-
-
-            /*Impresion con arreglo pero con errores
-            System.Console.WriteLine(arregloCursos[0].Nombre);
-            Console.WriteLine("Presione ENTER para continuar");
-            Console.ReadLine();
-            System.Console.WriteLine(arregloCursos[5].Nombre);
-             */
-            /* Impresion sin arreglo
-            Console.WriteLine(curso1.Nombre + ","+ curso1.UniqueId);
-            Console.WriteLine($"{curso2.Nombre}, {curso2.UniqueId}");
-            System.Console.WriteLine(curso3);
-            */
         }
+        private static void ImprimirCursosDoWhile(Curso[] arregloCursos)
+        {
+            int contador = 0;
+            do
+            {
+                Console.WriteLine($"Nombre {arregloCursos[contador].Nombre}, id{arregloCursos[contador].UniqueId}");
+                contador++;
+            }
+            while (contador < arregloCursos.Length);
+        }
+
+        private static void ImprimirCursosWhile(Curso[] arregloCursos)
+        {
+            int contador = 0;
+            while (contador < arregloCursos.Length)
+            {
+                Console.WriteLine($"Nombre {arregloCursos[contador].Nombre}, id{arregloCursos[contador].UniqueId}");
+                contador++;
+            }
+        }
+
+
+        /*Impresion con arreglo pero con errores
+        System.Console.WriteLine(arregloCursos[0].Nombre);
+        Console.WriteLine("Presione ENTER para continuar");
+        Console.ReadLine();
+        System.Console.WriteLine(arregloCursos[5].Nombre);
+         */
+        /* Impresion sin arreglo
+        Console.WriteLine(curso1.Nombre + ","+ curso1.UniqueId);
+        Console.WriteLine($"{curso2.Nombre}, {curso2.UniqueId}");
+        System.Console.WriteLine(curso3);
+        */
     }
+}
 
